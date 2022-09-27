a79e9ddbe8befb84eaf774ab378ef68b393adcd5
Jorge Lopes <jorgedclopes@gmail.com>
2022-09-27 10:46:24 +0100

    Add saves index file

--- src/crosschainMap/defaultLists/index.ts
+++ src/crosschainMap/defaultLists/index.ts
@@ -3,12 +3,10 @@ import { getArbitrumTokens } from './arbitrum'
 import { getOmnibridgeTokens } from './omnibridge'
 import { CrosschainMappingList } from '../../types'
 import { getOptimismTokens } from './optimism'
-// import { getSushiswapTokens } from './sushiswap'
 
 export const defaultLists: CrosschainMappingList[] = [
   getOmnibridgeTokens,
   getPolygonTokens,
   getArbitrumTokens,
   getOptimismTokens
-  //  getSushiswapTokens
 ]
